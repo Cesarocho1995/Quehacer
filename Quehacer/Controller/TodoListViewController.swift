862404f6ad52c123dfd75241df342ffa0a50545f
Cesar Mg <cesar_cobalt@hotmail.com>
2018-08-29 20:21:05 -0500

    Convert to MVC design pattern

--- Quehacer/Controller/TodoListViewController.swift
+++ Quehacer/Controller/TodoListViewController.swift
@@ -10,18 +10,30 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
-    var itemArray: [String] = []
+    var itemArray = [Item]()
     
     let defaults = UserDefaults.standard
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
+
+        let newItem = Item()
+        newItem.title = "Find Mike"
+        itemArray.append(newItem)
         
-        if let items = defaults.array(forKey: "TodoListArray") as? [String]
-        {
-            itemArray = items
-        }
+        let newItem1 = Item()
+        newItem1.title = "Hola"
+        itemArray.append(newItem1)
+        
+        let newItem2 = Item()
+        newItem2.title = "como estas?"
+        itemArray.append(newItem2)
+        
+//        if let items = defaults.array(forKey: "TodoListArray") as? [String]
+//        {
+//            itemArray = items
+//        }
         
     }
     
@@ -29,7 +41,7 @@ class TodoListViewController: UITableViewController {
     
     
 
-    //MARK - TableView Fatasource Methods
+    //MARK - TableView Datasource Methods
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
@@ -41,7 +53,19 @@ class TodoListViewController: UITableViewController {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         
-        cell.textLabel?.text = itemArray[indexPath.row]
+        cell.textLabel?.text = itemArray[indexPath.row].title
+        
+        cell.accessoryType = itemArray[indexPath.row].done == false ? .none : . checkmark
+
+//      LO MISMO QUE LA LINEA DE ARRIBA
+//        if itemArray[indexPath.row].done == false
+//        {
+//            cell.accessoryType = .none
+//        }
+//        else
+//        {
+//            cell.accessoryType = .checkmark
+//        }
         
         return cell
     }
@@ -55,14 +79,9 @@ class TodoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print(itemArray[indexPath.row])
         
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark
-        {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        }
-        else
-        {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+        tableView.reloadData()
         
         tableView.deselectRow(at: indexPath, animated: true  )
     }
@@ -86,7 +105,9 @@ class TodoListViewController: UITableViewController {
             
             if textField.text != ""
             {
-                self.itemArray.append(textField.text!)
+                let newItem = Item()
+                newItem.title = textField.text!
+                self.itemArray.append(newItem)
                 self.defaults.set(self.itemArray, forKey: "TodoListArray")
                 self.tableView.reloadData()
             }
---
+++ Quehacer/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Items.swift
+//  Quehacer
+//
+//  Created by Cesar Enrique Mora Guerra on 8/29/18.
+//  Copyright © 2018 Cesar Enrique Mora Guerra. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
